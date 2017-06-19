06a83b2703fb334f318a299a639360238337636a
Neo Lee <neo.lee@gmail.com>
2017-06-19 19:45:03 -0400

    Fix separator transparency issue

--- config/powerline-conf.el
+++ config/powerline-conf.el
@@ -2,6 +2,9 @@
 (live-add-pack-lib "powerline")
 (require 'powerline)
 
+; Fix the separator transparency issue
+(setq ns-use-srgb-colorspace nil)
+
 (powerline-default-theme)
 ; (powerline-center-theme)
 ; (powerline-center-evil-theme)
